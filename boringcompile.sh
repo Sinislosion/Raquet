0787e5cef6f3593ba8fa768d758e59b9557cf284
JESSIE T <jessieproductions@aol.com>
2023-12-12 12:16:46 -0800

    new stuffs

--- boringcompile.sh
+++ boringcompile.sh
@@ -7,7 +7,7 @@ echo "Building..."
 if [ -e "build/prog" ]; then
 	rm build/prog
 fi
-clang -std=c99 src/main.c -Wall -O2 -o build/prog -lSDL2 -I include/
+clang -std=c99 src/main.c -Wall -O2 -o build/prog `sdl2-config --cflags --libs` -I include/
 if [ -e "build/prog" ]; then
 	echo "Copying Assets"
 	cp -r assets build/
--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #define SDL_MAIN_HANDLED
-#include <SDL2/SDL.h>
+#include "SDL.h"
 #include <stdio.h>
 
 // WINDOW CONSTANTS

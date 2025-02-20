ca796e11558800116065c5fffcb1ee8178d9e8fb
Jessie J. T. <jessie@sinislosion.net>
2025-02-19 20:49:51 -0800

    ability to allocate and free CHRs, and some more restructure

--- include/Raquet/Raquet_Video.h
+++ include/Raquet/Raquet_Video.h
@@ -3,37 +3,6 @@
 
 /** @file */
 
-/** @brief The header for the PPF format */
-extern const unsigned char Raquet_PPFHeader[8];
-
-/** @brief The bitmask used for loading the individual bits from the PPF file */
-extern const unsigned int Raquet_PPFBitmask[8];
-
-/**
- * @brief Loads a PPF Bank into a PPF_Bank variable
- * @param targetarray The pointer to the PPF_Bank we want to load the file into
- * @param dir The path to the file
- */
-int Raquet_LoadPPFBank(PPF_Bank* targetarray, const char* dir);
-
-/**
- * @brief Loads a CHR texture from a PPF_Bank into a Raquet_CHR
- * @param ppfbank The PPF_Bank to load the texture from
- * @param id The CHR ID to load
- * @param pal The initial palette to use with the texture
- */
-Raquet_CHR Raquet_LoadCHR(PPF_Bank ppfbank, int id, Palette pal[4]);
-
-/**
- * @brief Loads a CHR texture from a PPF_Bank into a Raquet_CHR
- * @param ppfbank The PPF_Bank to load the texture from
- * @param id The array of CHR IDs to load ( -1 will load a blank 8x8 tile )
- * @param xwrap The amount of 8x8 tiles on the x axis
- * @param ywrap The amount of 8x8 tiles on the y axis
- * @param pal The initial palette to use with the texture
- */
-Raquet_CHR Raquet_LoadCHRMult(PPF_Bank ppfbank, int* id, int xwrap, int ywrap, Palette palette[4]);
-
 /**
  * @brief Initializes a palette during runtime in a single funciton
  * @param dest The palette to load the colors into
@@ -45,39 +14,41 @@ Raquet_CHR Raquet_LoadCHRMult(PPF_Bank ppfbank, int* id, int xwrap, int ywrap, P
 extern void Raquet_SetPalette(Palette dest[4], Uint32 pal1, Uint32 pal2, Uint32 pal3, Uint32 pal4);
 
 /**
- * @brief Places a CHR onto the screen with the x and y coordinates specified
- * @param chr The Raquet_CHR to place
- * @param x The Screen X Coordinate to place the texture
- * @param y The Screen Y Coordinate to place the texture
- */
-void Raquet_PlaceCHR(Raquet_CHR chr, int x, int y);
-
-/**
- * @brief Places a CHR onto the screen with the x and y coordinates specified
- * @param chr The Raquet_CHR to place
- * @param x The Screen X Coordinate to place the texture
- * @param y The Screen Y Coordinate to place the texture
- * @param xsize The width you wish to draw the texture in pixels
- * @param ysize The height you wish to draw the texture in pixels
- * @param angle The angle to rotate the sprite in radians
- * @param center The Raquet_Point to use as an origin point to rotate and scale the sprite around
- * @param flip The SDL_RendererFlip value of how the sprite should be flipped
- */
-void Raquet_PlaceCHR_ext(Raquet_CHR chr, int x, int y, int xsize, int ysize, double angle, Raquet_Point center, SDL_RendererFlip flip);
-
-/**
- * @brief Swaps a CHR's color palette with a different one
- * @param chr The pointer to the Raquet_CHR to modify
- * @param pal The new palette to use with the texture
- */
-void Raquet_SwapCHRPalette(Raquet_CHR* chr, Palette pal[4]);
-
-/**
  * @brief Copies a palette array to another palette array
  * @param dest The palette array to copy to
  * @param origin The palette array to copy from
  */
 extern void Raquet_CopyPalette(Palette dest[4], Palette origin[4]);
+
+/**
+ * @brief Wrapper for SDL's Render Set Draw Color, that converts a Raquet Palette Value to an RGBA8 value
+ * @param pal The palette color to set the Draw Color to
+ * @param alpha The opacity of the color ( 0 to 255 )
+ */
+extern void Raquet_SetDrawColor(Palette pal, int alpha);
+
+/* Clear the screen with a solid color */
+#ifdef BACKGROUND_CLEARING_COLOR
+    extern Palette Raquet_ClearColor;
+#endif
+
+/**
+ * @brief Clears the screen with a specified color
+ * @param pal The Palette color to clear the screen with.
+ */
+extern void Raquet_Clear(Palette pal);
+
+/**
+ * @brief Draws a rectangle of a given color onto the screen
+ * @param x The X position of the rectangle in Screen space
+ * @param y The Y position of the rectangle in Screen space
+ * @param width The width of the rectangle
+ * @param height The height of the rectangle
+ * @param pal The Palette color of the rectangle
+ * @param alpha The transparecny of the rectangle, with 255 being the max and 0 being the minimum
+ * @param fill Whether or not to fill in the rectangle (1 or yes, 0 for no)
+ */
+extern void Raquet_DrawRectangle(int x1, int y1, int width, int height, Palette pal, int alpha, int fill);
 
 /**
  * @brief Draws a single pixel onto the specified coordinates
@@ -98,11 +69,5 @@ void Raquet_DrawPoint(int x, int y, Palette pal, int alpha);
  * @param alpha The transparency of the line
  */
 void Raquet_DrawLine(int x1, int y1, int x2, int y2, Palette pal, int alpha);
-
-/**
- * @brief Frees a Raquet_CHR from memory
- * @param chr The pointer to the CHR you want to destroy
- */
-void Raquet_FreeCHR(Raquet_CHR* chr);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,170 +1,22 @@
 // put your libs here.
 #include "Raquet.h"
 
-// PPF Banks
+/*
+ ********************
+ *     PPF BANK     *
+ ********************
+*/
 PPF_Bank ppf_main;
-
-// Global Demo Vars
-float demotime = 0;
-
-// BG Stars
-#define PARTICLE_AMOUNT		SCREEN_WIDTH/4
-typedef struct Star {
-	int x, y;
-} Star;
-
-Star stararray[PARTICLE_AMOUNT];
-
-// Palettes
-Palette pal_face[4];
-Palette pal_face2[4];
-
-Palette pal_logo[4];
-
-// Characters
-Raquet_CHR chr_placeface;
-Raquet_CHR chr_raquetlogo_R;
-Raquet_CHR chr_raquetlogo_A;
-Raquet_CHR chr_raquetlogo_Q;
-Raquet_CHR chr_raquetlogo_U;
-Raquet_CHR chr_raquetlogo_E;
-Raquet_CHR chr_raquetlogo_T;
-
-/*
- **************************
- *     STAR FUNCTIONS     *
- **************************
-*/
-
-// Init each star particle
-Star createStars(void) {
-	int pos  = (rand() % (SCREEN_HEIGHT + 1));
-	Star par;
-	par.x = 0;
-	par.y = pos;
-
-	return par;
-}
-
-// init all our stars
-void initStars(void) {
-	for (int i = 0; i < PARTICLE_AMOUNT; i++) {
-		stararray[i] = createStars();
-		stararray[i].x = i * 4;
-	}
-}
-
-// draw the stars
-void drawStars(void) {
-
-	// for every star we know exists, and for every 4 points we need to draw.
-	for (int i = 0; i < PARTICLE_AMOUNT; i++) {
-		stararray[i].x -= 1;
-		for (int o = 0; o < 4; o++) {
-			int tempx = stararray[i].x;
-			Raquet_DrawPoint(tempx + o, stararray[i].y, Raquet_GlobalPalette[0x30], 255 - (64 * o));
-		}
-
-		// if a point exceeds 0, plus its length, loop it back to the screen width
-		if (stararray[i].x <= -4) {
-			stararray[i] = createStars();
-			stararray[i].x += SCREEN_WIDTH;
-		}
-	}
-}
-
-/*
- *********************
- *     PLACEFACE     *
- *********************
-*/
-Raquet_Actor* act_placeface;
-Raquet_Actor* act_placeface2;
-void bePlaceface(void) {
-	int key_up = Raquet_KeyCheck(SDL_SCANCODE_UP);
-	int key_down = Raquet_KeyCheck(SDL_SCANCODE_DOWN);
-	int key_left = Raquet_KeyCheck(SDL_SCANCODE_LEFT);
-	int key_right = Raquet_KeyCheck(SDL_SCANCODE_RIGHT);
-
-	int move_x = key_right - key_left;
-	int move_y = key_down - key_up;;
-
-	if (Raquet_ActorColliding(act_placeface->position.x + (move_x * 2), act_placeface->position.y, act_placeface, act_placeface2)) {
-		while (!Raquet_ActorColliding(act_placeface->position.x + Raquet_Sign(move_x), act_placeface->position.y, act_placeface, act_placeface2)) {
-			act_placeface->position.x += Raquet_Sign(move_x);
-		}
-		move_x = 0;
-	}
-
-	act_placeface->position.x += (move_x * 2);
-
-	if (Raquet_ActorColliding(act_placeface->position.x, act_placeface->position.y + (move_y * 2), act_placeface, act_placeface2))  {
-		while (!Raquet_ActorColliding(act_placeface->position.x, act_placeface->position.y + Raquet_Sign(move_y), act_placeface, act_placeface2)) {
-			act_placeface->position.y += Raquet_Sign(move_y);
-		}
-		move_y = 0;
-	}
-	act_placeface->position.y += (move_y * 2);
-
-	Raquet_DrawActor(act_placeface);
-	Raquet_DrawActor(act_placeface2);
-}
 
 /*
  **************************
  *     MAIN FUNCTIONS     *
  **************************
 */
-
 void createthedog(void)
 {
 	/* Graphical */
 	Raquet_LoadPPFBank(&ppf_main, Raquet_AbsoluteToAsset("main.ppf"));
-
-	/* Setup our palettes */
-	Raquet_SetPalette(pal_face, Raquet_GlobalPalette[0x0F], Raquet_GlobalPalette[0x0D], Raquet_GlobalPalette[0x00], Raquet_GlobalPalette[0x20]);
-	Raquet_SetPalette(pal_face2, Raquet_GlobalPalette[0x0F], Raquet_GlobalPalette[0x0D], Raquet_GlobalPalette[0x05], Raquet_GlobalPalette[0x25]);
-	Raquet_SetPalette(pal_logo, Raquet_GlobalPalette[0x0F], Raquet_GlobalPalette[0x20], Raquet_GlobalPalette[0x20], Raquet_GlobalPalette[0x20]);
-
-	/* Create our CHRs */
-
-	// placeface
-	chr_placeface = Raquet_LoadCHR(ppf_main, 0, pal_face);
-
-	// RAQUET logo
-	int arr_raquetlogo_R[2] = {1, 7};
-	int arr_raquetlogo_A[2] = {2, 8};
-	int arr_raquetlogo_Q[2] = {3, 9};
-	int arr_raquetlogo_U[2] = {4, 10};
-	int arr_raquetlogo_E[2] = {5, 11};
-	int arr_raquetlogo_T[2] = {6, 12};
-	chr_raquetlogo_R = Raquet_LoadCHRMult(ppf_main, arr_raquetlogo_R, 1, 2, pal_logo);
-	chr_raquetlogo_A = Raquet_LoadCHRMult(ppf_main, arr_raquetlogo_A, 1, 2, pal_logo);
-	chr_raquetlogo_Q = Raquet_LoadCHRMult(ppf_main, arr_raquetlogo_Q, 1, 2, pal_logo);
-	chr_raquetlogo_U = Raquet_LoadCHRMult(ppf_main, arr_raquetlogo_U, 1, 2, pal_logo);
-	chr_raquetlogo_E = Raquet_LoadCHRMult(ppf_main, arr_raquetlogo_E, 1, 2, pal_logo);
-	chr_raquetlogo_T = Raquet_LoadCHRMult(ppf_main, arr_raquetlogo_T, 1, 2, pal_logo);
-
-	/* Initialize our Stars */
-	initStars();
-
-	/* Create the Actors */
-	act_placeface = Raquet_AllocateActor();
-	Raquet_CreateActor(act_placeface, chr_placeface);
-
-	act_placeface2 = Raquet_AllocateActor();
-	Raquet_CreateActor(act_placeface2, chr_placeface);
-
-	/* Setup our actors */
-	act_placeface->position.x = SCREEN_WIDTH/2;
-	act_placeface->position.y = 64;
-
-	act_placeface2->width = 32;
-	act_placeface2->height = 32;
-	act_placeface2->bbox.x2 = act_placeface2->width;
-	act_placeface2->bbox.y2 = act_placeface2->height;
-	act_placeface2->position.x = 64;
-	act_placeface2->position.y = 64;
 
 	/* Audio */
 	Raquet_Sound snd_placeface = Raquet_LoadSound(Raquet_AbsoluteToAsset("2A03_Kevvviiinnn-Superfusion.wav"));
@@ -172,49 +24,8 @@ void createthedog(void)
 
 }
 
-void runthedog(void)
-{
-	demotime++;
-
-	// Draw our stuffs
-	Raquet_Clear(Raquet_GlobalPalette[0x12]);
-
-	drawStars();
-
-	for (int i = 0; i < 6; i++) {
-		Raquet_CHR arr[6] = {
-			chr_raquetlogo_R, chr_raquetlogo_A, chr_raquetlogo_Q,
-			chr_raquetlogo_U, chr_raquetlogo_E, chr_raquetlogo_T
-		};
-		Raquet_PlaceCHR(arr[i], ((SCREEN_WIDTH/2) - 24) + (8 * i) - Camera.x, ((SCREEN_HEIGHT/2) - 8) + (sin((demotime + (i * 8)) * .1) * 4) - Camera.y);
-	}
-
-	bePlaceface();
-
-	int move_x = Raquet_KeyCheck(SDL_SCANCODE_D) - Raquet_KeyCheck(SDL_SCANCODE_A);
-	int move_y = Raquet_KeyCheck(SDL_SCANCODE_S) - Raquet_KeyCheck(SDL_SCANCODE_W);
-
-	Camera.x += move_x;
-	Camera.y += move_y;
-
-	if (Raquet_KeyCheck_Pressed(SDL_SCANCODE_1)) {
-		Palette swap1 = Raquet_GlobalPalette[0x12];
-		Palette swap2 = Raquet_GlobalPalette[0x15];
-
-		Raquet_GlobalPalette[0x12] = swap2;
-		Raquet_GlobalPalette[0x15] = swap1;
-
-		Palette swap3[4];
-		Palette swap4[4];
-		Raquet_CopyPalette(swap3, pal_face);
-		Raquet_CopyPalette(swap4, pal_face2);
-
-		Raquet_CopyPalette(pal_face, swap4);
-		Raquet_CopyPalette(pal_face2, swap3);
-
-		Raquet_SwapCHRPalette(&act_placeface->chr, pal_face);
-	}
-
+void runthedog(void) {
+    Raquet_Clear(Raquet_GlobalPalette[0x0D]);
 }
 
 int main(void) {
